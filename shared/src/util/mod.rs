15d0684ea099b98895e6d8e058ef5085ccc28f18
lionrexhepi <94755628+lionrexhepi@users.noreply.github.com>
2023-01-22 18:47:48 +0100

    added boxable trait + blanket implementation

--- shared/src/util/mod.rs
+++ shared/src/util/mod.rs
@@ -1,3 +1,15 @@
-pub mod block_pos;
+pub mod pos;
 pub mod direction;
 pub mod movement;
+
+pub mod logger;
+
+pub trait Boxable {
+    fn boxed(self) -> Box<Self>;
+}
+
+impl<T> Boxable for T {
+    fn boxed(self) -> Box<Self> {
+        Box::new(self)
+    }
+}
