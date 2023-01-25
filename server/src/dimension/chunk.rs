8a5e8e1603c593635054502b1177e027d1985d2c
lionrexhepi <94755628+lionrexhepi@users.noreply.github.com>
2023-01-25 14:17:47 +0100

    i refactored

--- server/src/dimension/chunk.rs
+++ server/src/dimension/chunk.rs
@@ -2,28 +2,54 @@ use log::error;
 use metrohash::MetroHashMap;
 use shared::{ dimension::chunk::*, util::pos::ChunkPos };
 
-pub struct DiskChunkLoader;
+pub struct DiskChunkLoader {
+    save_folder: Path
+};
 
-trait ChunkGenerator: ChunkProvider {}
+impl DiskChunkLoader {
+    fn get_region_pos(chunk: &ChunkPos) -> (i32, i32) {
+        (
+            chunk.x() / 32,
+            chunk.z() / 32
+        )
+    }
+}
 
 impl ChunkProvider for DiskChunkLoader {
+
     fn get_chunk(&self, _pos: &shared::util::pos::ChunkPos) -> Result<Chunk, ChunkProviderError> {
         todo!()
     }
 }
 
+trait ChunkGenerator: ChunkProvider {}
+
 pub struct ServerChunkStorage {
     file_loader: DiskChunkLoader,
-    generator: dyn ChunkGenerator,
+    generator: Box<dyn ChunkGenerator>,
     chunk_map: MetroHashMap<u64, Chunk>,
 }
 
 impl ServerChunkStorage {
     pub const EMPTY_CHUNK: Chunk = Chunk {};
 
-    fn cache_chunk(&mut self, id: u64, chunk: Chunk) -> &Chunk {
-        self.chunk_map.insert(id, chunk);
-        self.chunk_map.get(&id).unwrap()
+    fn try_from_disk(&self, pos: &ChunkPos) -> Result<Chunk, bool> { //the error bool says whether you should try to generate the chunk first
+        match self.file_loader.get_chunk(pos) {
+            Ok(chunk) => Ok(chunk),
+            Err(e) => Err(e == ChunkProviderError::ChunkNotGeneratedError)
+        }
+       
+    }
+
+    fn try_generate(&self, pos: &ChunkPos) -> Result<Chunk, ()> {
+        match self.generator.get_chunk(pos) {
+            Ok(chunk) => Ok(chunk)
+            Err(err_type) => {
+                error!("Couldn't generate chunk! Error of type {:#?}", err_type);
+                debug_assert!(false);
+                Err(())
+            }
+        }
     }
 }
 
@@ -34,35 +60,16 @@ impl ChunkStorage for ServerChunkStorage {
 
     fn get_chunk(&mut self, pos: &ChunkPos) -> &Chunk {
         let id = pos.as_long();
-        {
-            if self.chunk_map.contains_key(&id) {
-                return self.chunk_map.get(&id).unwrap();
-            }
-        }
 
-        {
-            match self.file_loader.get_chunk(pos) {
-                Ok(chunk) => {
-                    return self.cache_chunk(id, chunk);
-                }
-                Err(e) => {
-                    let _ = match e {
-                        ChunkProviderError::ChunkNotGeneratedError => 0,
-                        _ => {
-                            return &Self::EMPTY_CHUNK;
-                        }
-                    };
-                }
+        self.chunk_map.entry(id).or_insert(self.try_from_disk(pos).or_else(|should_generate| {
+            if should_generate {
+                self.try_generate(pos).or(Self::EMPTY_CHUNK)
+            } else {
+                Self::EMPTY_CHUNK
             }
-        }
+        }))
 
-        match self.generator.get_chunk(pos) {
-            Ok(chunk) => { self.cache_chunk(id, chunk) }
-            Err(err_type) => {
-                error!("Couldn't generate chunk! Error of type {:#?}", err_type);
-                debug_assert!(false);
-                &Self::EMPTY_CHUNK
-            }
-        }
+
+        
     }
 }
