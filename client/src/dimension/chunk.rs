d0e5f9419b70dabf73b804ac32a315ee94c2900f
lionrexhepi <94755628+lionrexhepi@users.noreply.github.com>
2023-01-26 16:58:31 +0100

    added clientside chunk stuff

---
+++ client/src/dimension/chunk.rs
@@ -0,0 +1,19 @@
+use metrohash::MetroHashMap;
+pub use shared::dimension::chunk::*;
+use shared::util::pos::ChunkPos;
+
+
+pub struct ClientWorldStorage {
+    chunk_map: MetroHashMap<u64, Chunk>,
+    request_chunk_handler: dyn Fn(&ChunkPos),
+}
+
+impl ChunkStorage for ClientWorldStorage {
+    fn is_chunk_cached(&self, pos: &shared::util::pos::ChunkPos) -> bool {
+        self.chunk_map.contains_key(&pos.as_long())
+    }
+
+    fn get_chunk(&mut self, pos: &shared::util::pos::ChunkPos) -> &Chunk {
+        if let 
+    }
+}
