1dbb97d492ec2ced0bb6cb6a3ffdf5fd8efc0eca
lionrexhepi <94755628+lionrexhepi@users.noreply.github.com>
2023-01-26 17:22:01 +0100

    main antagonist introduction arc

--- server/src/dimension/chunk.rs
+++ server/src/dimension/chunk.rs
@@ -1,23 +1,21 @@
+use std::{ path::Path };
+
 use log::error;
 use metrohash::MetroHashMap;
 use shared::{ dimension::chunk::*, util::pos::ChunkPos };
 
 pub struct DiskChunkLoader {
-    save_folder: Path
-};
+    save_folder: Box<Path>,
+}
 
 impl DiskChunkLoader {
     fn get_region_pos(chunk: &ChunkPos) -> (i32, i32) {
-        (
-            chunk.x() / 32,
-            chunk.z() / 32
-        )
+        (chunk.x() / 32, chunk.z() / 32)
     }
 }
 
 impl ChunkProvider for DiskChunkLoader {
-
-    fn get_chunk(&self, _pos: &shared::util::pos::ChunkPos) -> Result<Chunk, ChunkProviderError> {
+    fn get_chunk(&self, _pos: &shared::util::pos::ChunkPos) -> Result<&Chunk, ChunkProviderError> {
         todo!()
     }
 }
@@ -33,17 +31,18 @@ pub struct ServerChunkStorage {
 impl ServerChunkStorage {
     pub const EMPTY_CHUNK: Chunk = Chunk {};
 
-    fn try_from_disk(&self, pos: &ChunkPos) -> Result<Chunk, bool> { //the error bool says whether you should try to generate the chunk first
+    /// the error bool says whether you should try to generate the chunk first
+    fn try_from_disk(&self, pos: &ChunkPos) -> Result<&Chunk, bool> {
         match self.file_loader.get_chunk(pos) {
             Ok(chunk) => Ok(chunk),
-            Err(e) => Err(e == ChunkProviderError::ChunkNotGeneratedError)
+            Err(e) =>
+                Err(matches!(e, ChunkProviderError::ChunkNotGeneratedError)),
         }
-       
     }
 
-    fn try_generate(&self, pos: &ChunkPos) -> Result<Chunk, ()> {
+    fn try_generate(&self, pos: &ChunkPos) -> Result<&Chunk, ()> {
         match self.generator.get_chunk(pos) {
-            Ok(chunk) => Ok(chunk)
+            Ok(chunk) => Ok(chunk),
             Err(err_type) => {
                 error!("Couldn't generate chunk! Error of type {:#?}", err_type);
                 debug_assert!(false);
@@ -61,15 +60,27 @@ impl ChunkStorage for ServerChunkStorage {
     fn get_chunk(&mut self, pos: &ChunkPos) -> &Chunk {
         let id = pos.as_long();
 
-        self.chunk_map.entry(id).or_insert(self.try_from_disk(pos).or_else(|should_generate| {
-            if should_generate {
-                self.try_generate(pos).or(Self::EMPTY_CHUNK)
-            } else {
-                Self::EMPTY_CHUNK
+        if let Some(c) = self.chunk_map.get(&id) {
+            return c;
+        }
+
+        match self.try_from_disk(pos) {
+            Ok(c) => {
+                return c;
             }
-        }))
+            Err(e) => {
+                if e {
+                    //Double it and give it to the next person
+                } else {
+                    return &Self::EMPTY_CHUNK;
+                }
+            }
+        };
 
+        match self.try_generate(pos) {
+            Ok(chunk) => chunk,
+            Err(_) => &Self::EMPTY_CHUNK
+        }
 
-        
     }
 }
